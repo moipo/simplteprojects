2b303cb4736a1d7f5c140e51268c7a51098396d6
moipo <moipochtoviyyashik@inbox.ru>
2022-07-09 15:17:06 +0300

    tried to add a slider, backend problems

--- everything/models.py
+++ everything/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Parametr(models.Model):
+    value = models.FloatField() #?
--- everything/views.py
+++ everything/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from .models import Parametr
 
 # Create your views here.
 
-def show_homepage(request, *args, **kwargs):     
-    return render(request, "homepage.html", {})
+def show_homepage(request, *args, **kwargs):
+
+    data = request.POST
+    val = 15
+    context = {
+        "data" : data,
+        "data1": val,
+    }
+    return render(request, "homepage.html", context)
+
+
+def show_ext(request, *args, **kwargs):
+
+    form = 15
+    context = {
+        'form' : form,    #передаем объект, который потом и используется в html как form.as_p
+    }
+    return render(request,"ext_home.html", context)
